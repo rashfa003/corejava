142bf9012b71b4103f6f482d94aca4661f5aec59
rashfa003 <rashfaparakandy03@gmail.com>
2025-02-26 19:34:27 +0530

    Initial commit

---
+++ project1/New folder/exc.java
@@ -0,0 +1,17 @@
+
+public class exc {
+    public static void main(String[] args) {
+        int i=9;
+        int j=0;
+
+        try{
+            j=18/i;
+        }
+        catch(Exception e){
+            System.out.println("something went wrong");
+        }
+
+        System.out.println(j);
+        System.out.println("bye");
+    }
+}
