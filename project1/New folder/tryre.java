9dd716e2d004c3b9b71343e4e8f253ede14e7b24
rashfa003 <rashfaparakandy03@gmail.com>
2025-02-27 20:11:16 +0530

    Initial commit

---
+++ project1/New folder/tryre.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class tryre {
+    public static void main(String[] args) throws IOException {
+        int num=0;
+        try(BufferedReader br =new BufferedReader(new InputStreamReader(System.in))){
+            num=Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
+    }
+}
